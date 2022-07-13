57ee0b7d3e76ca284ba875c7968f1ddd54dbbf92
曹玉东 <503577405@qq.com>
2022-07-13 18:05:37 +0800

    初始化vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// const app = createApp()
+// app.use(Vant)
+
+
+createApp(App).use(store).use(router).use(Vant).mount('#app')
